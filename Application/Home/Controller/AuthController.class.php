766c07f01879f99a05a59b52abf020545a9febe8
Zheng Li <645113150@qq.com>
2016-11-24 23:42:29 +0800

    sign up sameName detect

--- Application/Home/Controller/AuthController.class.php
+++ Application/Home/Controller/AuthController.class.php
@@ -6,4 +6,21 @@ class AuthController extends Controller {
         $this->show("login");
     }
 
+    public function signUp()
+    {
+        $this->display();
+    }
+
+    public function checkSameName()
+    {
+        $name = I('post.username');
+        $model = D("Auth");
+        $res = $model->checkSameName($name);
+        if ($res) {
+            echo '<text class="btn-danger">用户名重复，请重新输入</text>';
+            return;
+        }
+        echo '<text class="btn-success">用户名可用</text>';
+        return;
+    }
 }
--- Application/Home/Model/AuthModel.class.php
+++ Application/Home/Model/AuthModel.class.php
@@ -5,4 +5,14 @@ class AuthModel extends Model {
     public function __construct(){
     }
 
+
+    public function checkSameName($name)
+    {
+        $model = M("");
+        $res = $model->query("SELECT * FROM user_info WHERE user_name = '%s'",$name);
+        if ($res) {
+            return true;
+        }
+        return false;
+    }
 }
